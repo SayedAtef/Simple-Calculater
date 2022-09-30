e0bb761e20edc90752c25102371c9656dc3ac47b
Sayed <sayedatef6293@gmail.com>
2022-09-30 17:29:09 +0200

    change toggle button style

--- src/components/Toggle.jsx
+++ src/components/Toggle.jsx
@@ -22,41 +22,57 @@ const Toggle = () => {
 
   return (
     <div>
-      <div className="flex justify-center">
-        <p>theme</p>
-        <div className="flex flex-col">
-          <div className="flex justify-between">
-            <p>1</p>
-            <p>2</p>
-            <p>3</p>
-          </div>
-          <button
-            onClick={onClick}
-            className={`w-20 h-6 ${
-              one
-                ? "bg-t1-toggleAndKeyPadBackground"
-                : two
-                ? "bg-t2-toggleAndKeyPadBackground"
-                : "bg-t3-toggleAndKeyPadBackground"
-            } my-auto rounded-2xl flex ${
-              position === "second"
-                ? "justify-center"
-                : position === "third"
-                ? "justify-end"
-                : "justify-start"
-            }`}
-          >
-            <div
-              className={`rounded-full ${
-                one
-                  ? "bg-t1-keyBackgroundAndToggleRed"
-                  : two
-                  ? "bg-t2-keyBackgroundAndToggleRed"
-                  : "bg-t3-keyBackgroundAndToggleCyan"
-              } w-4 h-4  m-1`}
-            />
-          </button>
+      <div className="grid grid-rows-2 grid-flow-col gap-0">
+        <p
+          className={`row-span-2 self-end text-base mr-4 ${
+            one
+              ? "text-t1-textWhite"
+              : two
+              ? "text-t2-textBlue"
+              : "text-t3-textYellow"
+          }`}
+        >
+          THEME
+        </p>
+        <div
+          className={`row-span-1 text-sm flex justify-between ${
+            one
+              ? "text-t1-textWhite"
+              : two
+              ? "text-t2-textBlue"
+              : "text-t3-textYellow"
+          }`}
+        >
+          <p>1</p>
+          <p>2</p>
+          <p>3</p>
         </div>
+        <button
+          onClick={onClick}
+          className={`w-20 h-6 row-span-1 ${
+            one
+              ? "bg-t1-toggleAndKeyPadBackground"
+              : two
+              ? "bg-t2-toggleAndKeyPadBackground"
+              : "bg-t3-toggleAndKeyPadBackground"
+          } my-auto rounded-2xl flex ${
+            position === "second"
+              ? "justify-center"
+              : position === "third"
+              ? "justify-end"
+              : "justify-start"
+          }`}
+        >
+          <div
+            className={`rounded-full ${
+              one
+                ? "bg-t1-keyBackgroundAndToggleRed"
+                : two
+                ? "bg-t2-keyBackgroundAndToggleRed"
+                : "bg-t3-keyBackgroundAndToggleCyan"
+            } w-4 h-4  m-1`}
+          />
+        </button>
       </div>
     </div>
   );
